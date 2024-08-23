a727a0b6bec82a4414f096eba494c6a7f36476c2
Odion <odionfiona@gmail.com>
2024-08-23 11:59:04 +0300

    modified:   src/components/Details-earphones-1.jsx 	modified:   src/data.json

--- src/components/Details-earphones-1.jsx
+++ src/components/Details-earphones-1.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ProductData from '../data.json'
-import { Cartegory_1 } from './Cartegory-1'
-import { Cartegory_2 } from './Cartegory-2'
-import Earphone from '../Pages/Earphone';
-
-
+import React from "react";
+import ProductData from "../data.json";
+import { Cartegory_1 } from "./Cartegory-1";
+import { Cartegory_2 } from "./Cartegory-2";
+import Earphone from "../Pages/Earphone";
 
 export const Details_earphones = () => {
-  const earphones = ProductData.filter((product) => product.category === 'earphones');
+  const earphones = ProductData.filter(
+    (product) => product.category === "earphones"
+  );
   console.log(earphones.categoryImage);
 
   return (
     <div>
-      <div className="px-5 md:px-10 lg:px-[8 k%]">
+      <div className="px-5 md:px-10 lg:px-[8%] py-5">
         <div>
           <button
             className="px-4 bg-[#D87D4A] font-thin text-[#fff]"
@@ -23,54 +23,75 @@ export const Details_earphones = () => {
         </div>
 
         {/* mapping theough the product */}
-        <div>
-          <div>
-            {earphones.map((earphone) => {
-              return (
-                <div key={earphone.id}>
-                  <div>
-                    <img src={earphone.categoryImage.mobile} alt="product" />
+        <div className="lg:mt-4">
+          <div className=" flex max-sm:flex-col gap-10 ">
+            <div className=" mt-10">
+              {earphones.map((earphone) => {
+                return (
+                  <div key={earphone.id}>
+                    <div>
+                      <img
+                        className="md:hidden"
+                        src={earphone.categoryImage.mobile}
+                        alt="mobile picture"
+                      />
+                      <img
+                        className="max-sm:hidden lg:hidden xl:hidden"
+                        src={earphone.categoryImage.tablet2}
+                        alt="tablet photo"
+                      />
+                      <img
+                        className="max-sm:hidden max-lg:hidden w-[400px]"
+                        src={earphone.categoryImage.mobile}
+                        alt="desktop photo"
+                      />
+                    </div>
                   </div>
+                );
+              })}
+            </div>
+            {/* section 2 */}
+
+            <div className="">
+              <div className="py-5 md:ml-[80px] md:w-[339px]">
+                <h1 className="text-[#D87D4A] text-[14px] py-4">
+                  {" "}
+                  NEW PRODUCT{" "}
+                </h1>
+                <h1 className="font-primarybold text-[30px] lg:text-[40px] w-[200px] md:w-[200px] lg:w-[400px] leading-8 py-4">
+                  {earphones[0].name}{" "}
+                </h1>
+                <p className="text-[15px] sm:w-[100%] font-primaryregular py-2">
+                  {earphones[0].description}
+                </p>
+
+                <p> ${earphones[0].price}</p>
+
+                <div className="flex gap-4 py-4">
+                  <button
+                    className="p-3 w-[100px] bg-[#989696] cursor-pointer hover:bg-[#dedada]"
+                    type="button"
+                  >
+                    - 1 +
+                  </button>
+                  <button
+                    className="font-primaryregular text-[#fff] p-3 bg-[#D87D4A] text-[15px] cursor-pointer hover:bg-[#ab704d]"
+                    type="button"
+                  >
+                    ADD TO CART
+                  </button>
                 </div>
-              );
-            })}
-          </div>
-          {/* section 2 */}
-
-          <div>
-            <div className="py-5">
-              <h1 className="text-[#D87D4A] text-[14px] py-4"> NEW PRODUC </h1>
-              <h1 className="font-primarybold text-[30px] w-[200px] leading-8 py-4">
-                {earphones[0].name}
-              </h1>
-              <p className="text-[15px] font-primaryregular py-2">
-                {earphones[0].description}
-              </p>
-
-              <p> ${earphones[0].price}</p>
-
-              <div className="flex gap-4 py-4">
-                <button
-                  className="p-3 w-[100px] bg-[#989696] cursor-pointer hover:bg-[#dedada]"
-                  type="button"
-                >
-                  - 1 +
-                </button>
-                <button
-                  className="font-primaryregular text-[#fff] p-3 bg-[#D87D4A] text-[15px] cursor-pointer hover:bg-[#ab704d]"
-                  type="button"
-                >
-                  ADD TO CART
-                </button>
               </div>
             </div>
           </div>
 
           {/* features */}
 
-          <div>
-            <div>
-              <h1 className="text-[24px] font-primarymedium py-3">FEATURES</h1>
+          <div className="lg:flex mt-5 md:mt-10 lg:mt-[100px] lg:gap-[190px]">
+            <div className="lg:w-[635px]">
+              <h1 className="text-[24px] lg:text-[32px] font-primarymedium py-3">
+                FEATURES
+              </h1>
               <p className=" font-primaryregular text-[15px]">
                 Experience unrivalled stereo sound thanks to innovative acoustic
                 technology. With improved ergonomics designed for full day
@@ -89,67 +110,222 @@ export const Details_earphones = () => {
                 the popular classic black.
               </p>
             </div>
-          </div>
 
-          {/* IN THE BOX */}
+            {/* IN THE BOX */}
 
-          <div>
-            <div className=''>
-              <div>
-                <h1 className=" py-6 text-[24px]">IN THE BOX</h1>
-              </div>
+            <div className="">
+              <div className="flex max-sm:flex-col md:flex-row  lg:flex-col items-baseline md:gap-[150px] lg:gap-3">
+                <div>
+                  <h1 className=" py-6 text-[24px] lg:text-[32px]">
+                    IN THE BOX
+                  </h1>
+                </div>
 
-              <div>
-                <ul className="py-2 text-[15px] font-primaryregular">
-                  <li>
-                    <span className="text-[#D87D4A]">
-                      {earphones[0].includes[0].quantity}x
-                    </span>{" "}
-                    <span className="ml-4">
-                      {earphones[0].includes[0].item}
-                    </span>
-                  </li>
+                <div>
+                  <ul className="py-2 text-[15px] font-primaryregular">
+                    <li>
+                      <span className="text-[#D87D4A]">
+                        {earphones[0].includes[0].quantity}x
+                      </span>{" "}
+                      <span className="ml-4">
+                        {earphones[0].includes[0].item}
+                      </span>
+                    </li>
 
-                  <li>
-                    <span className="text-[#D87D4A]">
-                      {earphones[0].includes[1].quantity}x
-                    </span>{" "}
-                    <span className="ml-4">
-                      {earphones[0].includes[1].item}
-                    </span>
-                  </li>
-                  <li>
-                    <span className="text-[#D87D4A]">
-                      {earphones[0].includes[2].quantity}x
-                    </span>{" "}
-                    <span className="ml-4">
-                      {earphones[0].includes[2].item}
-                    </span>
-                  </li>
-                  <li>
-                    <span className="text-[#D87D4A]">
-                      {earphones[0].includes[3].quantity}x
-                    </span>{" "}
-                    <span className="ml-4">
-                      {earphones[0].includes[3].item}
-                    </span>
-                  </li>
-                  <li>
-                    <span className="text-[#D87D4A]">
-                      {earphones[0].includes[4].quantity}x
-                    </span>{" "}
-                    <span className="ml-4">
-                      {earphones[0].includes[4].item}
-                    </span>
-                  </li>
-                </ul>
+                    <li>
+                      <span className="text-[#D87D4A]">
+                        {earphones[0].includes[1].quantity}x
+                      </span>{" "}
+                      <span className="ml-4">
+                        {earphones[0].includes[1].item}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="text-[#D87D4A]">
+                        {earphones[0].includes[2].quantity}x
+                      </span>{" "}
+                      <span className="ml-4">
+                        {earphones[0].includes[2].item}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="text-[#D87D4A]">
+                        {earphones[0].includes[3].quantity}x
+                      </span>{" "}
+                      <span className="ml-4">
+                        {earphones[0].includes[3].item}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="text-[#D87D4A]">
+                        {earphones[0].includes[4].quantity}x
+                      </span>{" "}
+                      <span className="ml-4">
+                        {earphones[0].includes[4].item}
+                      </span>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
+
+        {/* Cartegory section */}
+
+        <div className="">
+          <div className=" md:mt-5 md:relative lg:relative lg:px-[%]">
+            <div className=" py-0 lg:mt-[100px]">
+              {/* first image */}
+              <img
+                className="md:hidden lg:hidden"
+                src="/src/assets/product-yx1-earphones/desktop/image-gallery-1.jpg"
+                alt="mobile"
+              />
+              <img
+                className="max-sm:hidden lg:hidden w-[277px] h-[174px] "
+                src="/src/assets/product-yx1-earphones/tablet/image-gallery-1.jpg"
+                alt="tablet"
+              />
+
+              <img
+                className="max-sm:hidden max-lg:hidden lg:h-[280px] lg:w-[492px]"
+                src="/src/assets/product-yx1-earphones/desktop/image-gallery-1.jpg"
+                alt="desktop"
+              />
+            </div>
+            {/* second image */}
+            <div className="py-8 lg:py-5 md:py-4">
+              <img
+                className="md:hidden lg:hidden "
+                src="/src/assets/product-yx1-earphones/desktop/image-gallery-2.jpg"
+                alt="mobile"
+              />
+              <img
+                className="max-sm:hidden lg:hidden  w-[277px] h-[174px]"
+                src="/src/assets/product-yx1-earphones/tablet/image-gallery-2.jpg"
+                alt="tablet"
+              />
+
+              <img
+                className="max-sm:hidden max-lg:hidden lg:h-[280px] lg:w-[492px]"
+                src="/src/assets/product-yx1-earphones/desktop/image-gallery-2.jpg"
+                alt="desktop"
+              />
+            </div>
+            {/* third image */}
+            <div className=" md:py-0 py-0 lg:absolute top-[-1px] lg:left-[48%] bg-[#1010101]">
+              <img
+                className="md:hidden lg:hidden "
+                src="/src/assets/product-yx1-earphones/desktop/image-gallery-3.jpg"
+                alt="mobile"
+              />
+              <img
+                className="max-sm:hidden lg:hidden md:absolute top-0 left-[42%] w-[395px] h-[368px] "
+                src="/src/assets/product-yx1-earphones/desktop/image-gallery-1.jpg"
+                alt="tablet"
+              />
+
+              <img
+                className="max-sm:hidden max-lg:hidden lg:h-[578px] lg:w-[630px]"
+                src="/src/assets/product-yx1-earphones/desktop/image-gallery-3.jpg"
+                alt="desktop"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* you may also like section */}
+        
+        <div className=" mt-[50px] lg:px-[%]">
+          <h1 className="font-primarymedium text-[25px]">YOU MAY ALSO LIKE</h1>
+          <div className="text-center py-4 md:flex gap-5">
+            <div className="md:py-5">
+              <img
+                className="md:hidden lg:hidden"
+                src="/src/shared/mobile/image-xx99-mark-one-headphones.jpg"
+                alt=""
+              />
+              <img
+                className="max-sm:hidden lg:hidden "
+                src="/src/shared/tablet/image-xx99-mark-one-headphones.jpg"
+                alt="tablet-image"
+              />
+              <img
+                className="max-sm:hidden max-lg:hidden"
+                src="/src/shared/desktop/image-xx99-mark-one-headphones.jpg"
+                alt="desktop-image"
+              />
+              <h2 className="font-primaryregular text-[20px] py-4">
+                XX99 MARK 1
+              </h2>
+              <button
+                className="bg-[#d87d4A] px-6 py-2 text-[#f1f1f1] text-[13px] hover:bg-[#c78c6a] "
+                type="button"
+              >
+                SEE PRODUCT
+              </button>
+            </div>
+
+            <div className="py-5">
+              <img
+                className=" md:hidden lg:hidden"
+                src="/src/shared/mobile/image-xx59-headphones.jpg"
+                alt=""
+              />
+              <img
+                className="max-sm:hidden lg:hidden"
+                src="/src/shared/tablet/image-xx59-headphones.jpg"
+                alt=""
+              />
+              <img
+                className="max-sm:hidden max-lg:hidden"
+                src="/src/shared/desktop/image-xx59-headphones.jpg"
+                alt=""
+              />
+              <h2 className="font-primaryregular text-[20px] py-4">XX59</h2>
+              <button
+                className="bg-[#d87d4A] px-6 py-2 text-[#f1f1f1] text-[13px] hover:bg-[#c78c6a]"
+                type="button"
+              >
+                SEE PRODUCT
+              </button>
+            </div>
+
+            <div className="py-5">
+              <img
+                className="md:hidden lg:hidden"
+                src="/src/shared/mobile/image-zx9-speaker.jpg"
+                alt=""
+              />
+              <img
+                className="max-sm:hidden lg:hidden"
+                src="/src/shared/tablet/image-zx9-speaker.jpg"
+                alt=""
+              />
+
+              <img
+                className="max-sm:hidden max-lg:hidden"
+                src="/src/shared/desktop/image-zx9-speaker.jpg"
+                alt="desktop"
+              />
+              <h2 className="font-primaryregular text-[20px] py-4">
+                ZX9 SPEAKER
+              </h2>
+              <button
+                className="bg-[#d87d4A] px-6 py-2 text-[#f1f1f1] text-[13px] hover:bg-[#c78c6a] "
+                type="button"
+              >
+                SEE PRODUCT
+              </button>
+            </div>
+          </div>
+          </div>
+
+
       </div>
       <Cartegory_1 />
       <Cartegory_2 />
     </div>
   );
-}
+};

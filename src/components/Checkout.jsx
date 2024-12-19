e7b627cf7e37fef217fefa0085bb70ddecf259d6
Odion <odionfiona@gmail.com>
2024-12-19 12:24:14 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/components/Checkout.jsx 	modified:   src/components/Others.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,77 +1,206 @@
-
-// import countryList from 'country-list'
+import { useState,useEffect } from "react"
+import countryList from 'country-list'
 
 const countries = countryList.getData();
 
 
 
 const Checkout = ({cartItems}) => {
+  // console.log(cartItems)
+
+    const [form, setForm] = useState({
+        Name: '',
+        email: '',
+        phone: '',
+        address: '',
+        zip: '',
+        city: '',
+        country: 'united Kingdom',
+        paymentMethod : '',
+        emoneypin: '',
+        emoneynumber: '',
+      });
+      
+      // console.log(form);
+
+    const [summary, setSummary] = useState({
+        subtotal: 0,    
+        shipping: 0,
+        vat: 0,
+        grandTotal: 0,});
+        
+        const [showPopup, setShowPopup] = useState();
+        const [popupMessage, setPopupMessage] = useState('');
+
+        useEffect(()=>{
+            const subtotal = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+            const vat = subtotal * 0.15;
+            const grandTotal = subtotal + summary.shipping + vat;
+            setSummary({
+                ...summary, 
+                subtotal,
+                vat,
+                grandTotal,
+            });
+        }, [cartItems])
 
 
+        const handleChange = (e) => {
+            const { name, value } = e.target;
+            setForm({
+              ...form,
+              [name]: value,
+            });
+          };
+
+          const handleCountryChange = (selectedOption) => {
+            setForm({
+              ...form,
+              country: selectedOption.value,
+            });
+          };
+
+          const handlePaymentMethodChange = (e) => {
+            const { value } = e.target;
+            setForm({
+              ...form,
+              paymentMethod: value,
+            });
+          };
+
+          const handleSubmit = () => {
+            if (
+              form.Name &&
+              form.email &&
+              form.phone &&
+              form.address &&
+              form.zip &&
+              form.city &&
+              form.country &&
+              (form.paymentMethod === 'e-Money' ? form.emoneyNumber && form.emoneyPin : true)
+            ) {
+              setPopupMessage('Order success');
+              setShowPopup(true);
+            } else {
+              setPopupMessage('Please enter billing details');
+              setShowPopup(true);
+            }
+          };
+
+          const countryOptions = countries.map((country) => ({
+            value: country.code,
+            label: country.name,
+          }));
+
+          const handlePopupClose = () => {
+            setShowPopup(false);
+          };
+        
     return(
-        <div>
+        <div className=" relative">
             <div className="mx-[180px]">
                 <div className="">
                   <h1 className="mt-10 py-10 font-primaryextrabold text-[36px]">CHECKOUT</h1>
                 </div>
-                <h2 className="text-[red] text-[20px] ">BILLING DETAILS</h2>
+                <h2 className="text-[#D87D4A] text-[20px] ">BILLING DETAILS</h2>
             </div> 
             <form action="" className="py-5">
                 <div className="mx-[180px] mt-10 flex justify-between w-1/2 mb-10">
                     <div className="">
                      <label htmlFor="" className=" block mb-4">Name</label>
-                     <input type="text"name="name"className=" px-5 py-4 border-2 w-[309px]"/>
+                     <input 
+                     type="text"name="Name"
+                     value={form.Name}
+                     onChange={handleChange}
+                     className=" px-5 py-4 border-2 w-[309px]"
+                     />
                     </div>
                     <div>
-                      <label htmlFor="" className=" block mb-4">Email Address</label>
-                      <input type="text"name="name" className="px-5 py-4 border-2  w-[309px]" />
+                      <label htmlFor="" className=" block mb-4">email</label>
+                      <input 
+                      type="email" 
+                      value={form.email}
+                      name="email" id=""
+                      onChange={handleChange}
+                      className="px-5 py-4 border-2 w-[309px]" />
                     </div>
                 </div>
                 <div className="mx-[180px]">
                     <label htmlFor=""className="block mb-4">phone Number</label>
-                    <input type="number" name="" className="px-5 py-4 border-2  w-[309px]"/>
+                    <input 
+                    type="number"name="phone" 
+                    value={form.phone}
+                    onChange={handleChange}
+                    className="px-5 py-4 border-2  w-[309px]"/>
                 </div>
             </form>
 
             {/* shipping Details */}
             <div className="">
                 <div className="mx-[180px] mt-[50px]">
-                    <h2 className="py-5 text-[red] text-[20px]">SHIPPING INFO</h2>
+                    <h2 className="py-5 text-[#D87D4A] text-[20px]">SHIPPING INFO</h2>
                     <div className="mt-10">
                         <label htmlFor=""className="block mb-2">Address</label>
-                        <input type="text" name="" className="px-5 py-5 border-2 w-[68%]"/>
+                        <input
+                         type="text" name="address" 
+                         value={form.address}
+                         onChange={handleChange}
+                         className="px-5 py-5 border-2 w-[68%]"/>
                     </div>
 
                     <div className="flex gap-[113px] mt-10">
                         <div>
                             <label htmlFor="" className="block mb-2">ZIP Code</label>
-                            <input type="tel" name="" className="px-5 py-5 border-2 w-[309px]" />
+                            <input 
+                            type="tel" name="zip" 
+                            value={form.zip}
+                            onChange={handleChange}
+                            className="px-5 py-5 border-2 w-[309px]" />
                         </div>
                         <div>
                             <label htmlFor="" className="block mb-2">City</label>
-                            <input type="text" name="" className="px-5 py-5 border-2 w-[309px]" />
+                            <input type="text" name="city" 
+                            value={form.city}
+                            onChange={handleChange}
+                            className="px-5 py-5 border-2 w-[309px]" />
                         </div>
                     </div>
 
                     <div className="mt-10">
-                        <label htmlFor="" className="block mb-2">country</label>
-                        <input type="text" name="" className="py-5 px-5 border-2 w-[309px]" />
+                        <label htmlFor="" className=" mb-2">country</label>
+                        <select
+                        option={countryOptions}
+                        value={{label:form.country, value:form.country}}
+                        onChange={handleCountryChange}
+                        />
+                        {/* <input type="text" name="" className="py-5 px-5 border-2 w-[309px]" /> */}
                     </div>
                 </div>
             </div>
 
             {/* Payment Details */}
             <div className="mt-[100px]">
-                <h2 className="mx-[180px] text-[red]">PAYMENT DETAILS</h2>
+                <h2 className="mx-[180px] text-[#D87D4A]">PAYMENT DETAILS</h2>
                 <div className="mt-[50px] flex justify-between mx-[180px] w-1/2">
                     <p>payment Method</p>
                     <div>
                       <div className="border-2 py-5 px-5 w-[300px]"> 
-                         <input type="radio" name="" className="w-5" />
-                         <label htmlFor=""className="ml-5">payment</label>
+                         <input 
+                         type="radio" 
+                         name="paymentMethod" 
+                         value="E.money"
+                         checked={form.paymentMethod ==="E.money"}
+                         onChange={handlePaymentMethodChange}
+                         className="w-5" />
+                         <label htmlFor=""className="ml-5">E.money</label>
                        </div>
                        <div className="border-2 py-5 px-5 w-[300px] mt-[30px]"> 
-                         <input type="radio" name="" className="w-5" />
+                         <input type="radio" 
+                         name="paymentMethod" 
+                         value="cash on Delivery"
+                         checked={form.paymentMethod === 'cash on Delivery'}
+                         onChange={handlePaymentMethodChange}
+                         className="w-5" />
                          <label htmlFor=""className="ml-5">Cash on Delivery</label>
                        </div>  
                     </div>
@@ -79,16 +208,80 @@ const Checkout = ({cartItems}) => {
 
                 <div className="mx-[180px] mt-10 flex justify-between w-1/2 mb-10">
                     <div className="">
-                     <label htmlFor="" className=" block mb-2">E.Money Number</label>
-                     <input type="number"name="name"className=" px-5 py-4 border-2 w-[309px]"/>
+                     <label htmlFor="" className=" block mb-2">e.Money Number</label>
+                     <input
+                      type="number"name="name"
+                      value={form.eMoneyNumber}
+                      onChange={handleChange}
+                      className=" px-5 py-4 border-2 w-[309px]"/>
                     </div>
                     <div>
                       <label htmlFor="" className=" block mb-2 px-3">e-Money PIN</label>
-                      <input type="number"name="name" className="px-5 py-4 border-2  w-[309px]" />
+                      <input 
+                      type="number"name="name" 
+                      value={form.eMoneyPin}
+                      onChange={handleChange}
+                      className="px-5 py-4 border-2  w-[309px]" />
                     </div>
                 </div>
             </div>
 
+            {/* SUMMARY */}
+            <div className="absolute top-0 right-[109px] mt-10">
+              <div>
+                <h1 className="text-[30px] font-primaryextrabold">SUMMARY</h1>
+                <div className="py-5">
+                  {cartItems.map((item,index) => (
+                    <div key={index} className="  xl:w-[300px] justify-between flex  items-center py-2">
+                      <div>
+                        <img src={item.image} alt="" className="w-[80px]" />
+                      </div>
+                      <div>
+                        <h2 className="text-[13px]">{item.name}</h2>
+                        <span>$ {item.price}</span>
+                      </div>
+                       <p>x{item.quantity}</p>
+                    </div>
+                  ))}
+
+                  {/* ToTal */}
+                  <div>
+                    <div className="flex justify-between mt-12">
+                      <h2 className="text-[20px] font-primarybold text-[#a3a0a0]">TOTAL</h2>
+                      <span>${summary.subtotal.toFixed()}</span>
+                    </div>
+                    <div className="flex justify-between mt-2">
+                      <h2 className="text-[20px] font-primarybold text-[#a3a0a0]">SHIPPING</h2>
+                      <span>${summary.shipping.toFixed()}</span>
+                    </div>
+                    <div className="flex justify-between mt-2">
+                      <h2 className="text-[20px] font-primarybold text-[#a3a0a0]">VAT (INCLUDED)</h2>
+                      <span>${summary.vat.toFixed()}</span>
+                    </div>
+                    <div className="flex justify-between mt-2 pb-10">
+                      <h2 className="text-[20px] font-primarybold text-[#a3a0a0]">GRAND TOTAL</h2>
+                      <span className="text-[#D87D4A]">${summary.grandTotal.toFixed()}</span>
+                    </div> 
+
+                    <button 
+                    onClick={handleSubmit}
+                    className="px-4 py-2 bg-[#D87D4A] text-[#fff] w-full"> CONTINUE & PAY</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              {showPopup && (
+                <div>
+                  <div>
+                    <h1>Payment Status</h1>
+                    <p>{popupMessage}</p>
+                  </div>
+                </div>
+
+              )}
+            </div>
         </div>
     )
 
--- src/components/Others.jsx
+++ src/components/Others.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
+// import React from 'react'
 
- export const Others = ({earphone, speaker}) => {
+//  export const Others = ({earphone, speaker}) => {
   
-    console.log(speaker);
-  return (
-    <div>
-      <div> 
-        <h1 className='py-[60px] mx-auto text-center text-[35px] font-primaryextrabold'>YOU MAY ALSO LIKE</h1>
-        <div>
-          <div className='flex gap-5 text-center px-[190px]'> 
-            {earphone.others.map((data, index) => {
-                return (
-                    <div key={index} className="">
-                      <div>
-                        <img src={data.image.desktop} alt="mobile" className='w-[350px] py-5' />
-                         <p className='pb-2'>{data.name}</p> 
-                         <div className='pb-[100px]'> 
-                           <button className='px-2 py-2 bg-[#D87D4A] text-[#fff]'>SEE PRODUCT</button>
-                         </div>
-                      </div>
+//     console.log(speaker);
+//   return (
+//     <div>
+//       <div> 
+//         <h1 className='py-[60px] mx-auto text-center text-[35px] font-primaryextrabold'>YOU MAY ALSO LIKE</h1>
+//         <div>
+//           <div className='flex gap-5 text-center px-[190px]'> 
+//             {earphone.others.map((data, index) => {
+//                 return (
+//                     <div key={index} className="">
+//                       <div>
+//                         <img src={data.image.desktop} alt="mobile" className='w-[350px] py-5' />
+//                          <p className='pb-2'>{data.name}</p> 
+//                          <div className='pb-[100px]'> 
+//                            <button className='px-2 py-2 bg-[#D87D4A] text-[#fff]'>SEE PRODUCT</button>
+//                          </div>
+//                       </div>
                         
-                    </div>
-                )
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+//                     </div>
+//                 )
+//             })}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
